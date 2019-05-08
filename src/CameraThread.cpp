4bda81fb4f35b3ee20eedbf99b5bee225b99842b
tommy1019 <tommy.stanley96@gmail.com>
2019-05-08 12:08:35 -0500

    Fixed memory name

--- src/CameraThread.cpp
+++ src/CameraThread.cpp
@@ -44,7 +44,7 @@ void* cameraThread(void* args)
     }
 
     //Open shared memory to sharedMemory
-    struct CAMData* sharedMemory = (struct CAMData*)openSharedMemory(LIDAR_MEMORY_NAME, sizeof(struct CAMData) * CAM_NUM_IMAGES);
+    struct CAMData* sharedMemory = (struct CAMData*)openSharedMemory(CAM_MEMORY_NAME, sizeof(struct CAMData) * CAM_NUM_IMAGES);
     if (sharedMemory == NULL)
     {
         writeDebugMessage("[CAM] Could not create shared memory.\n");
